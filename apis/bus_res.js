1e937f9c14309380f16b765aa43636a89fb48c9b
WLAngel <akloratok@gmail.com>
2017-07-19 18:13:35 +0800

    some basic things

--- apis/bus_res.js
+++ apis/bus_res.js
@@ -2,15 +2,21 @@
 // MOTC API:
 // http://ptx.transportdata.tw/MOTC#!/CityBusApi/
 
+module.exports = {
+  route: route,
+  stop: stop,
+}
+
 function route(City, RouteName) {
   // TODO go query MOTC api http://ptx.transportdata.tw/MOTC/v2/Bus/StopOfRoute/City/
   // and return stops of the RouteName's route
 
-
+  return City + ' ' + RouteName + ' route'
 }
 
 function stop(City, StopName) {
   // TODO go query MOTC api http://ptx.transportdata.tw/MOTC/v2/Bus/StopOfRoute/City/
   // and return all the routes passing through this stop
 
+  return City + ' ' + StopName + ' stop'
 }
--- index.js
+++ index.js
@@ -1,30 +1,94 @@
-const express = require('express')
+var express = require('express')
+var path = require('path')
+var bodyparser = require('body-parser')
+
+var bus = require('./apis/bus_res.js')
+var cities = require('./public/static/city.js')
+
+var app = express()
 
 
-var app = express();
+// this is a test resource
+var list = [
+  { "StopUID": "NWT149600",
+    "StopID": "149600",
+    "StopName": {
+      "Zh_tw": "力行新村",
+      "En": "Lixing New Village"
+    },
+    "RouteUID": "NWT16689",
+    "RouteID": "16689",
+    "RouteName": {
+      "Zh_tw": "793",
+      "En": "793"
+    },
+    "Direction": 0,
+    "EstimateTime": 49,
+    "MessageType": 0,
+    "SrcUpdateTime": "2017-07-19T17:59:50+08:00",
+    "UpdateTime": "2017-07-19T17:59:55+08:00",
+    "StopSequence": 5
+  },
+  { "StopUID": "NWT149601",
+    "StopID": "149601",
+    "StopName": {
+      "Zh_tw": "板橋榮家",
+      "En": "Banqiao Veterans Home"
+    },
+    "RouteUID": "NWT16689",
+    "RouteID": "16689",
+    "RouteName": {
+      "Zh_tw": "793",
+      "En": "793"
+    },
+    "Direction": 0,
+    "EstimateTime": 142,
+    "MessageType": 0,
+    "SrcUpdateTime": "2017-07-19T17:59:50+08:00",
+    "UpdateTime": "2017-07-19T17:59:55+08:00",
+    "StopSequence": 6
+  }]
 
-app.get('/routes', (req, res) => {
+
+
+app.set('views', path.join(__dirname, 'public', 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+
+app.post('/routes', (req, res) => {
   // TODO should return routes of bus
+  var city = req.body.City
+  var routename = req.body.RouteName
 
-  res.send('should return routes of bus')
+
+  res.render('routes', {
+    city,
+    routename,
+    list
+  })
 })
-app.get('/stops', (req, res) => {
+app.post('/stops', (req, res) => {
   // TODO should return RouteName of bus in a specific StopName
-
-  res.send('should return RouteName of bus in a specific StopName')
+  var city = req.body.City
+  var stopname = req.body.StopName
+  res.render('stops', {
+    city,
+    stopname,
+  })
 })
 
 app.get('/util', (req, res) => {
-  // TODO should show utilities
-
-  res.send('should show utility options for user to play with')
+  res.render('util.pug', cities)
 })
 
 // TODO root page reserved
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+  res.send('The Page is Reserved!')
+})
 
 app.listen(8888, function () {
-  console.log('Example app listening on port 8888!');
-});
+  console.log('Example app listening on port 8888!')
+})
